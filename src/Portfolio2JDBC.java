dcf45245683c96249cf43d44eb6145df02cc86d5
Andreas Work <andreas-work@hotmail.com>
2020-03-28 16:07:11 +0100

    more code

--- src/Portfolio2JDBC.java
+++ src/Portfolio2JDBC.java
@@ -5,20 +5,32 @@ import java.util.Scanner;
 public class Portfolio2JDBC {
 
     public static void main(String[] args) {
+        System.out.println("1 or 2");
+Scanner oneortwo = new Scanner(System.in);
+String choosePath = oneortwo.nextLine();
 
-    }
+
+if (choosePath.equals("1")) {
+    getAverageStudentGrade();
+        }
+
+else if (choosePath.equals("2")) {
+    getAverageCourseGrade();
+}
+}
 
     private static void getAverageStudentGrade() {
         //
         Connection connect = null;
-        String url = "jdbc:sqlite:CoursePort.sqlite";
+        String url = "jdbc:sqlite:/Users/awork/Documents/Portfolio2Database/SqlDatabase.sqlite";
 
         try {
             connect = DriverManager.getConnection(url);
             //
             Statement statement = connect.createStatement();
             System.out.println("Type in the studentID");
-            Scanner scanner = new scanner.nextInt();
+            Scanner scanner = new Scanner(System.in);
+            int studentID = scanner.nextInt();
 
             //
             String query = "SELECT AVG(gradeResult) FROM grade WHERE studentID =" + studentID;
@@ -38,6 +50,35 @@ public class Portfolio2JDBC {
     }
 
     private static void getAverageCourseGrade() {
+        Connection connection = null;
+        String url = "jdbc:sqlite:/Users/awork/Documents/Portfolio2Database/SqlDatabase.sqlite";
+
+        try {
+            connection = DriverManager.getConnection(url);
+            //
+            Statement stat = connection.createStatement();
+            System.out.println("Type in the coureID");
+            Scanner scan = new Scanner(System.in);
+            int courseID = scan.nextInt();
+
+            //
+            String queryCourse = "SELECT AVG(gradeResult) FROM grade WHERE courseID =" + courseID;
+
+            //
+            ResultSet resultCourse = stat.executeQuery(queryCourse);
+
+            //
+            /*
+            if (courseID >= 1 && courseID <= 10) {
+                System.out.println(resultCourse.getFloat(1));
+            } else {
+                System.out.println("Can´t find CourseID");
+            }
+
+             */
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
